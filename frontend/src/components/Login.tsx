e56a56be4ead1814195dbc04e90aee17250ce770
Sen Harald Hjort Inao <senharald07@gmail.com>
2024-06-02 17:40:14 +0200

    added css animations

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -3,10 +3,18 @@ import "./Login.css"
 const Login = () => {
 	return (
 		<div className="Loginpage">
-			<form>
-				<input type="text"></input>
-				<button></button>
-			</form>
+      <div className="inputform-container">
+        <div className="input-container">
+          <label id="username-text">Username</label>
+          <input type="text" id="username-input"/>
+        </div>
+
+        <div className="input-container">
+          <label id="password-text">Password</label>
+          <input type="text" id="password-input"/>
+        </div>
+        <button>Login</button>
+      </div>
 		</div>
 	);
 };
